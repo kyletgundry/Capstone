c5778c48b2301c7c6012f5931d0b13a26a8e5bde
Kyle Gundry <Gundr1kt@gmail.com>
2017-04-12 21:41:05 -0500

    Update code to take in all sources if Source is nil

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,13 @@
 class ArticlesController < ApplicationController
 
   def index
+    @news_all = []
+    sources = Source.all
+    sources.each do |source_all|
+      @news_all << source_all[:source]
+    end
     @news = []
-
-    @sources = params["sources"]
+    @sources = params["sources"] || @news_all
       @sources.each do |source|
         result = Unirest.get("https://newsapi.org/v1/articles?source=#{source}&sortBy=top&apiKey=01372794d65d437b88b19d238dab8f89").body
         @news << result
